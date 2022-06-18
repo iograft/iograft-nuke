0b8313da5a39e315f134b923d7fcb7cf3ae4913d
Dan Knowlton <dan@iograft.com>
2022-06-17 20:54:42 -0700

    Update naming of IOG_NUKE_EXE to IOG_NUKE_COMMAND

--- bin/iognuke_subcore.bat
+++ bin/iognuke_subcore.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:: Pull the Nuke EXE from an environment variable. This EXE should
+:: Pull the Nuke command from an environment variable. This command should
 :: be set as an environment variable in the environment settings to
 :: allow for easily changing the Nuke version.
-"%IOG_NUKE_EXE%" -t "%~dp0\iognukepy_subcore.py" %*
+"%IOG_NUKE_COMMAND%" -t "%~dp0\iognukepy_subcore.py" %*
